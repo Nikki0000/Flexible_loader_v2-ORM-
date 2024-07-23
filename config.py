b0929d7aba659ca70249bb5242918dcf0a0c253c
Savodyarkin Nikita <snikitafut@mail.ru>
2024-07-23 15:02:13 +0300

    update

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Файл конфигурации для параметров подключения к базе данных
 #db_connection = 'postgresql://username:password@localhost/dbname'
 
-db_connection = 'postgresql://savodyarkin_n:gsXAww8kIAiz3EVM@172.27.10.11:6432/pharma-demo'
+db_connection = 'postgresql://username:password@localhost/dbname'
